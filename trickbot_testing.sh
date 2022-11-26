d07e3261bda2ed709a1b3d5f6454adf6e2a942eb
Nobel-CS <ashiques16@gmail.com>
2022-11-25 18:39:37 -0600

    more trickbot

--- trickbot_testing.sh
+++ trickbot_testing.sh
@@ -12,14 +12,17 @@ fi
 
 ####### Packets ########
 
-zip_stream_list="$(tshark -r $1 	-Y "(http.request or ssl.handshake.type == 1) 
+zip_stream_list="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) 
 				and !(ssdp) 
 				and http.request.uri contains "zip"" -O tcp,http -l| grep "Stream index" | awk 'match($0, /([0-9]+)/, matches) {print matches[1]}')"
 				# 
-						
+# Export http objects
+#tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x --export-object "http,malware_export"
+# [Make sure there is no space between http,malware_export or there will be issues.]
+					
 for stream_num in $zip_stream_list
 do
-zip_stream="$(tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x --export-object "")"
+zip_stream="$(tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x)"
 echo "$zip_stream"
 done
 
@@ -39,6 +42,9 @@ done
 #URL filter:		-Y "http.request.uri contains "/google/""
 #tcp conv show:		-z conv,tcp
 #Output view:		-O tcp,http
+#Output pkt separator:	-S "###" 
+#Output fields(!view):	-T fields -e ip.addr
+#
 
 
 
