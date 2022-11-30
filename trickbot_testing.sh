71402008ec377c60042111709b9ebf60f91ea0c6
Nobel-CS <ashiqurs16@gmail.com>
2022-11-30 17:04:08 -0600

    Pre-Infection tests - complete

--- trickbot_testing.sh
+++ trickbot_testing.sh
@@ -12,20 +12,25 @@ fi
 
 ####### Pre-Infection Analysis ########
 
+echo "Pre-Infection Checks:" > report.txt #Start New Report (>> will append)
+
+
+#_#_#_#_#_#_#_#_#_ Hash Check _#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+
 # Get tcp stream index (that contains zip in GET request)
 zip_stream_list="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) 
 				and !(ssdp) 
-				and http.request.uri contains "zip"" -O tcp,http -l| grep "Stream index" | awk 'match($0, /([0-9]+)/, matches) {print matches[1]}')"
-				# 
+				and http.request.uri contains "zip"" -O tcp,http -l | grep "Stream index" | awk 'match($0, /([0-9]+)/, matches) {print matches[1]}')"
+				# -T fields -e tcp.stream
 # Export http objects
-(tshark -2 -r $1 --export-object "http,malware_check")
+#Run Only Once -- otherwise it will make duplicate
+#(tshark -2 -r $1 --export-object "http,malware_check")
 #tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x --export-object "http,malware_export"
 # [Make sure there is no space between http,malware_export or there will be issues.]
 
-# Show tcp streams					
+# Check tcp streams for zip file requests
 for stream_num in $zip_stream_list
 do
-
 
 zip_stream="$(tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x)"
 
@@ -40,13 +45,50 @@ hash_2="$(sha512sum $zip_file | awk -F" " '{print $1}')"
 
 if [[ $hash_1 -eq $hash_2 ]]
 then
-echo
-echo "<<<<<<<<<<< Malware Infected Zip file found. >>>>>>>>>>>"
+echo "Hash Check:" >> report.txt
+echo "In stream: $zip_file - Hash: $hash_2" >> report.txt
+echo "Malware  : $malware_file - Hash: $hash_1" >> report.txt
+echo >> report.txt
+echo >> report.txt
+#echo "<<<<<<<<<<< Malware Infected Zip file found. >>>>>>>>>>>"
 fi
 
 done
 
+# Get tcp stream index for malicious host ip
+# exe file from 144.91.69.195 is not exported by --export-object
+mal_stream_list="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) and !(ssdp) and http.host==144.91.69.195" -O http,tcp -l -T fields -e tcp.stream)"
+for stream_num in $mal_stream_list
+do
 
+mal_stream="$(tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x)"
+
+mal_stream_info="$(echo "$mal_stream" | grep "Follow" -A28)"
+echo "Malicious Stream Info:" >> report.txt
+echo "$mal_stream_info" >> report.txt
+done
+###
+
+#_#_#_#_#_#_#_#_#_ Hash Check _#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+
+
+
+#_#_#_#_#_#_#_#_#_ Host Check _#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
+malicious_requests="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) 
+				and !(ssdp) 
+				and (http.host==144.91.69.195 or http.host==www.dchristjan.com)" -O http -l -T fields -e http.host)"
+malicious_request_count="$(echo "$malicious_requests" |wc -l)"
+				
+if [[ $malicious_request_count -gt 0 ]]
+then
+echo >> report.txt
+echo "Host check:" >> report.txt
+echo "No. of requests to malicious site: $malicious_request_count" >> report.txt
+echo $malicious_requests >> report.txt
+echo >> report.txt
+echo >> report.txt
+fi
+#_#_#_#_#_#_#_#_#_ Host Check _#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
 
 
 
@@ -67,6 +109,7 @@ done
 
 #### list of filters and regular expressions
 
+#Two pass:		-2
 #General filter:	-Y "(http.request or ssl.handshake.type == 1) and !(ssdp)"
 #URL filter:		-Y "http.request.uri contains "/google/""
 #tcp conv show:		-z conv,tcp
@@ -91,5 +134,5 @@ done
 
 #echo $allARP
 
-
+(cat report.txt)
 
