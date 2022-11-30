cc5a2fae3d3681790640a65b20c1b673ae5d9733
Nobel-CS <ashiqurs16@gmail.com>
2022-11-30 11:34:01 -0600

    pre_infection_check_2

--- trickbot_testing.sh
+++ trickbot_testing.sh
@@ -10,7 +10,7 @@ fi
 
 #echo "Packet Analysis Result:"
 
-####### Packets ########
+####### Pre-Infection Analysis ########
 
 # Get tcp stream index (that contains zip in GET request)
 zip_stream_list="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) 
@@ -18,18 +18,43 @@ zip_stream_list="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1)
 				and http.request.uri contains "zip"" -O tcp,http -l| grep "Stream index" | awk 'match($0, /([0-9]+)/, matches) {print matches[1]}')"
 				# 
 # Export http objects
-(tshark -2 -r $1 --export-object "http,malware_export")
+(tshark -2 -r $1 --export-object "http,malware_check")
 #tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x --export-object "http,malware_export"
 # [Make sure there is no space between http,malware_export or there will be issues.]
 
 # Show tcp streams					
 for stream_num in $zip_stream_list
 do
+
+
 zip_stream="$(tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x)"
-echo "$zip_stream"
+
+
+malware_file="malware_export/dd05ce3a-a9c9-4018-8252-d579eed1e670.zip"
+
+zip_directory='malware_check/'
+zip_file="$zip_directory$(echo "$zip_stream" | grep -oP '(?<=GET /).*(?=HTTP)')"
+
+hash_1="$(sha512sum $malware_file | awk -F" " '{print $1}')"
+hash_2="$(sha512sum $zip_file | awk -F" " '{print $1}')"
+
+if [[ $hash_1 -eq $hash_2 ]]
+then
+echo
+echo "<<<<<<<<<<< Malware Infected Zip file found. >>>>>>>>>>>"
+fi
+
 done
 
 
+
+
+
+####### Post-Infection Analysis ########
+
+
+
+#grep -oP '(?<=GET /).*(?=HTTP)'
 #-z follow,http,ascii,7
 #-O tcp -S "###" 
 #-Y "(http.request or ssl.handshake.type == 1) and !(ssdp)" 
