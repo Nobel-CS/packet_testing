e5ff66007b8ef5edad6a2ef738e4d2d1fde1b48a
Nobel-CS <ashiqurs16@gmail.com>
2022-12-07 16:58:36 -0600

    Pre infection +

--- trickbot_pre_infection.sh
+++ trickbot_pre_infection.sh
@@ -20,8 +20,9 @@ echo "Pre-Infection Checks:" > report.txt #Start New Report (>> will append)
 # Get tcp stream index (that contains zip in GET request)
 zip_stream_list="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) 
 				and !(ssdp) 
-				and http.request.uri contains "zip"" -O tcp,http -l | grep "Stream index" | awk 'match($0, /([0-9]+)/, matches) {print matches[1]}')"
+				and http.request.uri contains "zip"" -O tcp,http -l | grep "Stream index" | awk 'match($0, /([0-9]+)/) {matches = substr($0, RSTART, RLENGTH); print matches}')"
 				# -T fields -e tcp.stream
+
 # Export http objects
 if [[ -d "malware_check" ]]; then
 	if [[ "$(ls -A malware_check)" ]]; then
@@ -50,7 +51,7 @@ echo "******************************************" >> report.txt
 echo "Exported Object: $zip_file - Hash: $object_hash" >> report.txt
 ## the next command executes early for some reason ##
 vt_result="$(vt file $object_hash)"
-echo "Virustotal - detection: $(echo "$vt_result" | grep "malicious:" | awk 'match($0, /([0-9]+)/, matches) { print matches[1] }' | awk '{ sum += $1 } END { print sum }') sources detected malware presence in file."
+echo "Virustotal - detection: $(echo "$vt_result" | grep "malicious:" | awk 'match($0, /([0-9]+)/) {matches = substr($0, RSTART, RLENGTH); print matches}' | awk '{ sum += $1 } END { print sum }') sources detected malware presence in file."
 echo "******************************************" >> report.txt
 echo >> report.txt
 echo >> report.txt
