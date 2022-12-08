f853e6a93112cb84e87d83b6e5e6ef4c33fd612a
Nobel-CS <ashiqurs16@gmail.com>
2022-12-07 19:37:36 -0600

    Post infection tests

--- trickbot_post_infection.sh
+++ trickbot_post_infection.sh
@@ -8,7 +8,92 @@ fi
 
 ###### Post-Infection Analysis ########
 
+<<COMPLETED_BLOCK
 
-malicious_port_act="$(tshark -r $1 -Y "(ssl.handshake.type == 11 and x509sat.uTF8String=="Some-State")" -O tcp,http -l -x)"
+#################### Checking certificate ###############################
 
-echo "$malicious_port_act"
+malicious_cert="$( tshark -nr $1 -2 -R "ssl.handshake.certificates and frame contains "State""  -T fields -e x509sat.uTF8String )"
+echo "Irregular Digital Certificate : "
+echo "$malicious_cert" | uniq
+
+#########################################################################
+#    api.ip.sb
+#    checkip.amazonaws.com
+#    icanhazip.com
+#    ident.me
+#    ip.anysrc.net
+#    ipecho.net
+#    ipinfo.io
+#    myexternalip.com
+#   wtfismyip.com
+#########################################################################
+
+
+
+################### Checking File type variation ########################
+
+echo 
+mal="$( tshark -nr $1 -2 -R "http.request.method contains GET and http.request.uri contains ".png" and frame contains ".exe" " -T fields -e ip.src )"
+echo "Malicious File Download from : (exe as png)"
+echo "$mal" | uniq
+
+COMPLETED_BLOCK
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##<<TEST_BLOCK
+#################### TESTING ######################
+#This program cannot be run in DOS mode.#
+
+##### 
+#### matches function requires '' closure and is case insensitive
+echo "$(tshark -n -r $1 -Y 'http.request.method matches "(get|post)"')"
+
+
+#########################################################################
+
+mal_stream_list="$(tshark -nr $1 -Y '(http.request or ssl.handshake.type == 1) and !(ssdp) and http.request.method matches "(get|post)" and http.request.uri contains ".png"' -O http,tcp -l -T fields -e tcp.stream)"
+for stream_num in $mal_stream_list
+do
+echo "******************************************" 
+
+mal_stream="$(tshark -2 -nr $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x)"
+mal_stream_info="$(echo "$mal_stream" | grep "Follow" -A28)"
+echo "Malicious Stream Info:" 
+echo "$mal_stream_info" 
+
+
+
+echo "******************************************"
+done
+##TEST_BLOCK
--- trickbot_pre_infection.sh
+++ trickbot_pre_infection.sh
@@ -18,7 +18,7 @@ echo "Pre-Infection Checks:" > report.txt #Start New Report (>> will append)
 #_#_#_#_#_#_#_#_#_ Hash Check _#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
 
 # Get tcp stream index (that contains zip in GET request)
-zip_stream_list="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) 
+zip_stream_list="$(tshark -nr $1 -Y "(http.request or ssl.handshake.type == 1) 
 				and !(ssdp) 
 				and http.request.uri contains "zip"" -O tcp,http -l | grep "Stream index" | awk 'match($0, /([0-9]+)/) {matches = substr($0, RSTART, RLENGTH); print matches}')"
 				# -T fields -e tcp.stream
@@ -40,7 +40,7 @@ echo "Hash Check:" >> report.txt
 
 for stream_num in $zip_stream_list
 do
-zip_stream="$(tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x)"
+zip_stream="$(tshark -2 -nr $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x)"
 
 zip_directory='malware_check/'
 zip_file="$zip_directory$(echo "$zip_stream" | grep -oP '(?<=GET /).*(?=HTTP)')"
@@ -51,44 +51,26 @@ echo "******************************************" >> report.txt
 echo "Exported Object: $zip_file - Hash: $object_hash" >> report.txt
 ## the next command executes early for some reason ##
 vt_result="$(vt file $object_hash)"
-echo "Virustotal - detection: $(echo "$vt_result" | grep "malicious:" | awk 'match($0, /([0-9]+)/) {matches = substr($0, RSTART, RLENGTH); print matches}' | awk '{ sum += $1 } END { print sum }') sources detected malware presence in file."
+echo "Virustotal - detection: $(echo "$vt_result" | grep "malicious:" | awk 'match($0, /([0-9]+)/) {matches = substr($0, RSTART, RLENGTH); print matches}' | awk '{ sum += $1 } END { print sum }') sources detected malware presence in file." >> report.txt
 echo "******************************************" >> report.txt
 echo >> report.txt
 echo >> report.txt
 
 done
 
-#malware_file="malware_export/dd05ce3a-a9c9-4018-8252-d579eed1e670.zip"
-#hash_1="$(sha256sum $malware_file | awk -F" " '{print $1}')"
-#if [[ "$hash_1" == "$hash_2" ]]; then
-#echo "Malware Object: $malware_file - Hash: $hash_1" >> report.txt
-#echo "<<<<<<<<<<< Malware Infected Zip file found. >>>>>>>>>>>"
-#fi
+
+
+###########################
 
 
 
-# Get tcp stream index for malicious host ip
-# exe file from 144.91.69.195 is not exported by --export-object
-mal_stream_list="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) and !(ssdp) and http.host==144.91.69.195" -O http,tcp -l -T fields -e tcp.stream)"
-for stream_num in $mal_stream_list
-do
-echo "******************************************" >> report.txt
-
-mal_stream="$(tshark -2 -r $1 -Y "tcp.stream eq $stream_num" -z follow,tcp,ascii,$stream_num -x)"
-mal_stream_info="$(echo "$mal_stream" | grep "Follow" -A28)"
-echo "Malicious Stream Info:" >> report.txt
-echo "$mal_stream_info" >> report.txt
-
-echo "******************************************" >> report.txt
-done
-###
 
 #_#_#_#_#_#_#_#_#_ Hash Check _#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
 
 
 
 #_#_#_#_#_#_#_#_#_ Host Check _#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#_#
-malicious_requests="$(tshark -r $1 -Y "(http.request or ssl.handshake.type == 1) 
+malicious_requests="$(tshark -nr $1 -Y "(http.request or ssl.handshake.type == 1) 
 				and !(ssdp) 
 				and (http.host==144.91.69.195 or http.host==www.dchristjan.com)" -O http -l -T fields -e http.host)"
 malicious_request_count="$(echo "$malicious_requests" |wc -l)"
